1f314cf7efb395a6b18cd21d51c0bd1c88591ac6
周伟建 <zhouweijian@travelsky.com>
2021-01-06 09:40:16 +0800

    addTest

---
+++ src/main/java/com/yahoo/egads/control/Test.java
@@ -0,0 +1,21 @@
+package com.yahoo.egads.control;
+
+/**
+ * 功能描述:
+ * @author Zhou Weijian 
+ * @Date 2021年01月06日 09:39
+ * @version 1.0
+ * @Company 中国民航信息网络股份有限公司
+ * @Document <a></a>
+ * @History
+ * @Copyright  © 2017-2020 by TravelSky Limited.
+ * All rights reserved. 
+ * <br>
+ * This software is the confidential and propertary information of TravelSky Limited.
+ * ("Confidential Information").<br>
+ * You shall not disclose TravelSky Confidential Information and shall use it only in accordance
+ * the terms of the license<br>
+ * aggreement you entered into With TravelSky.
+ */
+public class Test {
+}
